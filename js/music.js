619584cfa7874b4a644430155eef527000f152d8
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2021-01-16 23:30:13 +0100

    working mute icon

--- js/music.js
+++ js/music.js
@@ -7,3 +7,19 @@ function soundsLoaded() {
 
 loop.addUri("/music/music.mp3", 23145, "music");
 loop.callback(soundsLoaded);
+
+var icon = document.getElementById('soundIcon');
+
+let isMute = false
+
+icon.onclick = function () {
+    if (isMute) {
+        isMute = false
+        icon.src = "/images/sound.svg"
+        loop.start("music");
+    } else {
+        isMute = true
+        icon.src = "/images/no-sound.svg"
+        loop.stop("music");
+    }
+}; 
