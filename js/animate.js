ebf48b3018d21ce4796d1c036cdbe8723cfe4074
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2021-01-16 18:14:02 +0100

    working planet model

--- js/animate.js
+++ js/animate.js
@@ -1,8 +1,8 @@
-const animate = function () {
-    requestAnimationFrame(animate);
+// const animate = function () {
+//     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+//     // cube.rotation.x += 0.01;
+//     // cube.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
-};
+//     renderer.render(scene, camera);
+// };
--- js/app.js
+++ js/app.js
@@ -1,15 +1,76 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let scene, camera, renderer;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function init() {
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x222222);
 
-camera.position.z = 5;
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+    // camera.rotation.y = 45 / 180 * Math.PI;
+    // camera.position.x = 7;
+    // camera.position.y = 10;
+    // camera.position.z = 10;
 
-animate();
+
+    camera.position.x = -7.8587422370910645;
+    camera.position.y = 4.857544898986816;
+    camera.position.z = -16.959041595458984;
+    camera.rotation.x = 1.6317866451706384;
+    camera.rotation.y = -0.018295476787527132;
+    camera.rotation.z = 2.7127579845176455;
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', renderer);
+
+    hlight = new THREE.AmbientLight(0x404040, 3);
+    scene.add(hlight);
+
+    // directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    // directionalLight.position.set(0, 1, 0);
+    // directionalLight.castShadow = true;
+    // scene.add(directionalLight);
+
+    // directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    // directionalLight.position.set(0, -1, 0);
+    // directionalLight.castShadow = true;
+    // scene.add(directionalLight);
+
+    light = new THREE.PointLight(0xc4c4c4, 1);
+    light.position.set(0, 300, 500);
+    scene.add(light);
+    light2 = new THREE.PointLight(0xc4c4c4, 1);
+    light2.position.set(500, 100, 0);
+    scene.add(light2);
+    light3 = new THREE.PointLight(0xc4c4c4, 1);
+    light3.position.set(0, 100, -500);
+    scene.add(light3);
+    light4 = new THREE.PointLight(0xc4c4c4, 1);
+    light4.position.set(-500, 300, 500);
+    scene.add(light4);
+
+    // light = new THREE.PointLight(0xc4c4c4, 10);
+    // light.position.set(0, 300, 500);
+    // scene.add(light);
+
+    document.body.appendChild(renderer.domElement);
+    let loader = new THREE.GLTFLoader();
+
+    loader.load('models/low-poly-planet.glb', function (gltf) {
+        gltfScene = gltf.scene;
+
+        console.log(gltfScene)
+        // car.scale.set(0.5, 0.5, 0.5);
+        scene.add(gltf.scene);
+        animate();
+    });
+}
+
+function animate() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+init();
