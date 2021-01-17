a7170de46cf80922ec0839ddf8bb01e7a68b9619
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2021-01-17 15:51:50 +0100

    music from three.js

--- js/music.js
+++ js/music.js
@@ -1,25 +1,30 @@
-var loop = new SeamlessLoop();
-
-function soundsLoaded() {
-    loop.start("music");
-    loop.volume(0.3);
-};
-
-loop.addUri("/music/music.mp3", 23145, "music");
-loop.callback(soundsLoaded);
-
-var icon = document.getElementById('soundIcon');
-
 let isMute = false
 
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add(listener);
+
+// create a global audio source
+const sound = new THREE.Audio(listener);
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load('/music/music.mp3', function (buffer) {
+    sound.setBuffer(buffer);
+    sound.setLoop(true);
+    sound.setVolume(0.3);
+    sound.play();
+});
+
+var icon = document.getElementById('soundIcon');
 icon.onclick = function () {
     if (isMute) {
         isMute = false
         icon.src = "/images/sound.svg"
-        loop.start("music");
+        sound.play();
     } else {
         isMute = true
         icon.src = "/images/no-sound.svg"
-        loop.stop("music");
+        sound.pause();
     }
 }; 
